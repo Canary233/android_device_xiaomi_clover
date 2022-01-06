95165130e3dbb112c2f85d53c0468a3cca7bb05f
Canary233 <2096276316@qq.com>
2022-01-06 10:22:17 +0800

    clover:SHRP Support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,3 +79,13 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+#SHRP_Variables
+SHRP_DEVICE_CODE := clover
+SHRP_PATH := device/xiaomi/clover
+SHRP_MAINTAINER :=  Canary233
+SHRP_REC_TYPE :=  Treble
+SHRP_DEVICE_TYPE :=  SAR
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_EDL_MODE := 0
+SHRP_FLASH := 0
